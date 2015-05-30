04a842b5e9a1c7aa64803c0406cd479129dc99fa
gattazr <remi.gattaz@gmail.com>
2015-05-31 01:42:37 +0200

    Modification des tests
    - Ajout de deux tests pour le dictionnaire
    - Ajout d'un makefile pour compiler les programmes de tests

--- tests/test_dico.c
+++ tests/test_dico.c
@@ -4,10 +4,11 @@
 
 
 int main(){
-	
+
 	Code a, b, c, d, e, f, g, h, i;
 	char * s = NULL;
-	
+	int wI;
+
 	initialiser();
 	a = inserer(82,69);
 	c = existe_code(259);
@@ -18,16 +19,33 @@ int main(){
 	h = inserer(261,72);
 	i = inserer(262,65);
 	s = codeVersSequence(263);
-	
+
 	printf("inserer(67,79) : %d\n", a);
 	printf("existe_code(259) : %d\n", c);
 	printf("inserer(259,85) : %d\n", d);
 	printf("existe_code(260) : %d\n", e);
 	printf("existe_seq(259,65,&b) : %d\n", f);
 	printf("inserer(260,84) : %d\n", g);
-	printf("inserer(261,72) : %d\n", g);
-	printf("inserer(262,65) : %d\n", g);
+	printf("inserer(261,72) : %d\n", h);
+	printf("inserer(262,65) : %d\n", i);
 	printf("codeVersSequence(263) : %s\n", s);
-	
+
+	/* longue séquence pour tester codeVersSequence */
+	b = 'B';
+	a = 'A';
+	for(wI = 0; wI < 64; wI++){
+		a = inserer(a, b);
+		b++;
+	}
+	s = codeVersSequence(a);
+	printf("codeVersSequence(%d) : %s\n", a, s);
+
+	/* Très longue séquence séquence */
+	a = 0;
+	for(wI = 0; wI < 1000; wI++){
+		a = inserer(a, b);
+		b++;
+	}
+
 	return 0;
 }
